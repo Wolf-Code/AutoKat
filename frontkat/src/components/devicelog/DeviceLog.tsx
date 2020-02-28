7ab4bb28fd0f7b6d4f05c253bab334c8925f6812
Kevin Deelen <kevin.deelen@gmail.com>
2020-02-28 17:22:58 +0100

    feat: adds bootstrap to improve logs page

--- frontkat/src/components/devicelog/DeviceLog.tsx
+++ frontkat/src/components/devicelog/DeviceLog.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, useState, useEffect } from 'react'
+import { Table } from 'reactstrap'
 import { useFetch } from '../../core/utils/useFetch'
 import { DeviceLog } from '../../domain/devicelog/DeviceLog'
 import { useSocket } from '../../contexts/socket/SocketContext'
@@ -29,14 +30,25 @@ export const DeviceLogComponent: FunctionComponent<DeviceLogComponentProps> = pr
 	}
 
 	return (
-		<>
-			{logs
-				?.sort((a, b) => new Date(b.moment).getTime() - new Date(a.moment).getTime())
-				.map((log, index) => (
-					<div key={index}>
-						{new Date(log.moment).toLocaleString()}: [{log.level}] {log.message}
-					</div>
+		<Table>
+			<thead>
+				<tr>
+					<th>Time</th>
+					<th>Level</th>
+					<th>Message</th>
+				</tr>
+			</thead>
+			<tbody>
+				{logs
+					?.sort((a, b) => new Date(b.moment).getTime() - new Date(a.moment).getTime())
+					.map((log, index) => (
+						<tr key={index}>
+							<td>{new Date(log.moment).toLocaleString()}</td>
+							<td>{log.level}</td>
+							<td>{log.message}</td>
+						</tr>
 				))}
-		</>
+			</tbody>
+		</Table>
 	)
 }
